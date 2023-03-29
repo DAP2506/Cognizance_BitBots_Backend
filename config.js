c737f1d3fa2944b41c7afab205d335e98003e28a
dhruv <dapanchal2506@gmail.com>
2023-03-30 00:15:48 +0530

    config added

---
+++ config.js
@@ -0,0 +1,42 @@
+// const host = 'phamd.cphgcqpntpen.ap-south-1.rds.amazonaws.com';
+// const mysql_user = 'admin';
+// const mysql_password = 'Cognizance_PHAMD';
+// const mysql_db = 'phamd';
+// const mysql_port = 3306;
+// const connection_pool = 50;
+// const port = 5000;
+// const tokenKey = 'ham_iit_roorkee_jaa_rahe_hai_cognizance_ke_hackathon_me';
+
+
+// export { host, mysql_user, mysql_db, mysql_password, mysql_port, connection_pool, port, tokenKey };
+// ========================================================================================================
+
+const host = 'sql12.freesqldatabase.com';
+const mysql_user = 'sql12609612';
+const mysql_password = 'i8CFGc1pBk';
+const mysql_db = 'sql12609612';
+const mysql_port = 3306;
+const connection_pool = 50;
+const port = 5000;
+const tokenKey = 'ham_iit_roorkee_jaa_rahe_hai_cognizance_ke_hackathon_me';
+
+
+export { host, mysql_user, mysql_db, mysql_password, mysql_port, connection_pool, port, tokenKey };
+
+
+// ========================================================================================================
+
+// import dotenv from "dotenv"
+// dotenv.config()
+
+// const host = process.env.host;
+// const mysql_user = process.env.mysql_user;
+// const mysql_password = process.env.mysql_password;
+// const mysql_db = process.env.mysql_db;
+// const mysql_port = 3306;
+// const connection_pool = 50;
+// const port = 5000;
+// const tokenKey = process.env.tokenKey;
+
+
+// export { host, mysql_user, mysql_db, mysql_password, mysql_port, connection_pool, port, tokenKey };
